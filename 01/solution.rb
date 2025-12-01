7e0ab0e8dedcad7e6976df4f9acd39d6cc169da6
Ptolemy Barnes <ptolemybarnes@gmail.com>
2025-12-01 23:57:37 +0100

    solves day 1: part 2

--- 01/solution.rb
+++ 01/solution.rb
@@ -1,20 +1,60 @@
-class Dial
-  attr_reader :current
+class CyclicalRange
+  include Enumerable
 
-  def initialize
-    @current = 50
+  def initialize(start, finish, direction)
+    @start = start
+    @finish = finish
+    @direction = direction
+  end
+
+  def self.forwards(start, finish)
+    new(start, finish, :forwards)
+  end
+
+  def self.backwards(start, finish)
+    new(start, finish, :backwards)
+  end
+
+  def each
+    a = @start
+    b = @finish
+    if @direction == :forwards
+      while a < b
+        a += 1
+        yield a % 100
+      end
+    else
+      while a > b
+        a -= 1
+        yield a % 100
+      end
+    end
+  end
+end
+
+class Dial
+  attr_reader :history
+
+  def initialize(starting)
+    @history = starting
+  end
+
+  def head
+    history.last
   end
 
   def wind_forwards(n)
-    @current = (@current + n) % 100
+    range = CyclicalRange.forwards(head, (head + n))
+    Dial.new([*history, *range.to_a])
   end
 
   def wind_backwards(n)
-    @current = (@current - n) % 100
+    range = CyclicalRange.backwards(head, (head - n))
+    Dial.new([*history, *range.to_a])
   end
 
-  def zero?
-    @current == 0
+  def to_s
+    "<Dial { #{history.join(", ")} }>"
   end
 end
 
@@ -33,20 +73,22 @@ class Rotation
   def backwards?
     !forward?
   end
+
+  def to_s
+    "<Rotation: #{forward? ? 'R' : 'L'} - #{number_of_clicks}>"
+  end
 end
 
 input = File.readlines('./input.txt')
-rotations = input.map {|line| Rotation.new(line) }
+rotations = input.map(&:rstrip).reject(&:empty?).map {|line| Rotation.new(line) }
 
-dial = Dial.new
-
-zeros_count = rotations.count do |rotation|
+final_dial = rotations.reduce(Dial.new([50])) do |dial, rotation|
   if rotation.forward?
     dial.wind_forwards(rotation.number_of_clicks)
   else 
     dial.wind_backwards(rotation.number_of_clicks)
   end
-  dial.zero?
 end
 
-puts zeros_count
+
+puts final_dial.history.count(&:zero?)
