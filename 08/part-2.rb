9546a9da06ba82d59be52276f5996a1fae41bb44
Ptolemy Barnes <ptolemybarnes@gmail.com>
2026-01-19 19:38:36 +0100

    completes day 8

--- 08/part-2.rb
+++ 08/part-2.rb
@@ -1,3 +1,5 @@
+require 'benchmark' 
+
 class JunctionBox
   attr_accessor :connections
 
@@ -16,6 +18,9 @@ class JunctionBox
     if @connections.include? other
       return self
     end
+    if self == other
+      return self
+    end
     @connections << other
     other.connect(self)
     self
@@ -25,9 +30,10 @@ class JunctionBox
     "<Box x={#{x}} y={#{y}} z={#{z}} connections_count={#{@connections.count}}>"
   end
   
-  def get_boxes_in_circuit(connections_to_exclude = [])
-    connections_connections = (@connections - connections_to_exclude).map do |connection|
-      connection.get_boxes_in_circuit([self, *connections_to_exclude])
+  def get_boxes_in_circuit(connections_to_exclude)
+    connections_to_exclude.add(self)
+    connections_connections = (Set.new(@connections) - connections_to_exclude).map do |connection|
+      connection.get_boxes_in_circuit(connections_to_exclude)
     end
     [self, *connections_connections.flatten].uniq
   end
@@ -40,14 +46,27 @@ class JunctionBox
 end
 
 def get_closest_pair_that_are_not_connected(pairs)
-  pairs.find do |pair|
+  result = pairs.find do |pair|
     a, b = pair
     !(a == b || a.connected?(b))
   end
+  puts "Closest pair is at index #{pairs.index(result)}/#{pairs.count}"
+  result
+end
+
+def _are_all_boxes_connected?(boxes)
+  count_of_boxes_in_circuit = boxes.first.get_boxes_in_circuit(Set.new).size
+  puts "Boxes in circuit: #{count_of_boxes_in_circuit}"
+  count_of_boxes_in_circuit == boxes.size
 end
 
 def are_all_boxes_connected?(boxes)
-  (boxes - boxes.first.get_boxes_in_circuit).empty?
+  result = nil
+  time = Benchmark.measure do
+    result = _are_all_boxes_connected?(boxes)
+  end
+  puts "Method call time: #{time}"
+  return result
 end
 
 boxes = File.readlines("./input.txt").map do |line|
@@ -59,8 +78,13 @@ box_pairs_sorted_by_distance = boxes.product(boxes).sort_by do |pair|
   a.distance_to(b)
 end
 
-until are_all_boxes_connected? do
+i = 0
+until are_all_boxes_connected?(boxes)
+  puts "Iteration: #{i}"
+  i = i.next
   box_a, box_b = get_closest_pair_that_are_not_connected(box_pairs_sorted_by_distance)
-  box_a.connect(box_b)
+  puts "Connecting boxes #{box_a.inspect} to #{box_b.inspect}"
+  last = box_a.connect(box_b)
+  puts "Last: #{box_a.x * box_b.x}"
 end
 
