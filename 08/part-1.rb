8a5eafa872ecda0a85daa03173bb5b89fb35e0f4
Ptolemy Barnes <ptolemybarnes@gmail.com>
2026-01-19 17:24:30 +0100

    completes part 1

--- 08/part-1.rb
+++ 08/part-1.rb
@@ -1,15 +1,79 @@
-class JunctionBoxPosition < Struct.new(:x, :y, :z)
+class JunctionBox
+  attr_accessor :connections
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+    @connections = []
+  end
 
   def distance_to(other)
-    (x - other.x).abs + (y - other.y).abs + (z - other.z).abs
+    Math.sqrt(((x - other.x) ** 2) + ((y - other.y) ** 2) + ((z - other.z) **2))
+  end
+
+  def connect(other)
+    if @connections.include? other
+      return self
+    end
+    @connections << other
+    other.connect(self)
+    self
+  end
+
+  def inspect
+    "<Box x={#{x}} y={#{y}} z={#{z}} connections_count={#{@connections.count}}>"
+  end
+  
+  def get_boxes_in_circuit(connections_to_exclude = [])
+    connections_connections = (@connections - connections_to_exclude).map do |connection|
+      connection.get_boxes_in_circuit([self, *connections_to_exclude])
+    end
+    [self, *connections_connections.flatten].uniq
+  end
+
+  def connected?(other)
+    @connections.include?(other)
+  end
+
+  attr_reader :x, :y, :z
+end
+
+def get_closest_pair_that_are_not_connected(_boxes)
+  _boxes.product(_boxes).min_by do |pair|
+    a, b = pair
+    if a == b || a.connected?(b)
+      Float::INFINITY
+    else
+      a.distance_to(b)
+    end
   end
 end
 
-boxes = File.readlines("./test-input.txt").map do |line|
-  JunctionBoxPosition.new(*line.split(',').map(&:to_i))
+boxes = File.readlines("./input.txt").map do |line|
+  JunctionBox.new(*line.split(',').map(&:to_i))
 end
 
-position_a = boxes[0]
-position_b = boxes[1]
+1000.times do |i|
+  puts "Iteration: #{i}"
+  box_a, box_b = get_closest_pair_that_are_not_connected(boxes)
+  box_a.connect(box_b)
+end
 
-p position_a.distance_to(position_b)
+circuits = []
+remaining_boxes = boxes.dup
+
+until remaining_boxes.empty? do
+  box = remaining_boxes.first
+  boxes_in_circuit = box.get_boxes_in_circuit
+  circuits << boxes_in_circuit
+  remaining_boxes = remaining_boxes - box.get_boxes_in_circuit
+end
+
+circuit_sizes = circuits.map(&:size).sort.reverse
+puts "Count of circuits: #{circuits.count}"
+puts "Circuit sizes: #{circuit_sizes.join(", ")}"
+
+puts "Product of 3 largest circuits: #{circuit_sizes.take(3).reduce(:*)}"
+
+
